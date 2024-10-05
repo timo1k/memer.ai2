8369704f088d5e5f267e3ea7294333301f90b878
Timothy <timkhumpan@gmail.com>
2024-10-05 13:24:22 -0400

    create ui skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,10 +16,50 @@ import { View } from "@adobe/react-spectrum";
 // import SearchForm from "./SearchForm";
 // import CategoryList from "./CategoryList";
 // import AboutAddOn from "./AboutAddOn";
+import Fab from "@mui/material/Fab";
 import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import NavigationIcon from "@mui/icons-material/Navigation";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 import "./App.css";
 
 const App = memo(({ addOnSdk }) => {
+  const itemData = [
+    {
+      img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+      title: "Breakfast",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+      title: "Burger",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+      title: "Camera",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+      title: "Coffee",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+      title: "Hats",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+      title: "Honey",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+      title: "Basketball",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+      title: "Fern",
+    },
+  ];
+
   return (
     <View
       width="100%"
@@ -28,9 +68,54 @@ const App = memo(({ addOnSdk }) => {
       position="relative"
       backgroundColor="default"
     >
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <View>
+        <ButtonGroup variant="contained" aria-label="Basic button group">
+          <Button>Video</Button>
+          <Button>Photo</Button>
+        </ButtonGroup>
+      </View>
+
+      <View>
+        <View>
+          <View>Tiktok</View>
+          <Fab variant="extended">
+            <NavigationIcon sx={{ mr: 1 }} />
+            tiktok
+          </Fab>
+        </View>
+        <View>
+          <View>LockedIn</View>
+          <Fab variant="extended">
+            <NavigationIcon sx={{ mr: 1 }} />
+            lockedin
+          </Fab>
+        </View>
+        <View>
+          <View>Instagram</View>
+          <Fab variant="extended">
+            <NavigationIcon sx={{ mr: 1 }} />
+            insta
+          </Fab>
+        </View>
+      </View>
+
+      <View>
+        <View>Template Videos</View>
+        <View>
+          <ImageList sx={{ width: 250, height: 450 }} cols={2} rowHeight={164}>
+            {itemData.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                  alt={item.title}
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </View>
+      </View>
     </View>
   );
 });
